590ca3c9d021c0fe2c9c415043d22b7ec6c4465e
Christopher Shelton <seashell86@users.noreply.github.com>
2024-08-05 20:48:56 -0400

    Added environment variable to it.

--- hello-world.py
+++ hello-world.py
@@ -1,7 +1,11 @@
 import flask
+import os
+
 
 app = flask.Flask(__name__)
 
+MESSAGE = f'Hello, {os.getenv('HELLO_WORLD_NAME', 'World')}!'
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return MESSAGE
